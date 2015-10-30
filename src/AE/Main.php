41ca7ad8a4d9615cbc719b4a4bb3aa504101c9cd
SavionLegendZzz <saviondeavault@gmail.com>
2015-10-30 17:55:46 -0400

    Update Main.php

--- src/AE/Main.php
+++ src/AE/Main.php
@@ -7,7 +7,6 @@ use pocketmine\command\Command;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\command\ConsoleCommandSender;
