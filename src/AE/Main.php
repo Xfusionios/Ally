85a79aad51d155b61b6ecfd2c56b42eb5e912e28
SavionLegendZzz <saviondeavault@gmail.com>
2015-11-03 07:00:11 -0500

    Update Main.php

--- src/AE/Main.php
+++ src/AE/Main.php
@@ -27,19 +27,34 @@ public function onPlayerLogin(PlayerPreLoginEvent $event){
         $file = ($this->getDataFolder()."Players/".$ign.".yml");  
             if(!file_exists($file)){
                 $this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
-                $this->PlayerFile->set("Ban","false");
+                $this->PlayerFile->set($player->getName()." Allies!");
                 $this->PlayerFile->save();
             }
         }
         
-        public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
  if(strtolower($cmd->getName()) === "ally") {
             if(isset($args[0]) && isset($args[1])){
                 $name = $args[0];
                 $target = $this->getServer()->getPlayer($name);
-                 $target->sendMessage($sender->getName()." Wants to be allys!");
+                if($sender instanceof Player){
+                if($target === null){
+                	$sender->sendMessage("that player is not online!");
+                	return false;
+                }
+                if($target instanceof Player){
+                $sender->sendMessage("Ally request sent to".$target->getName());
+                 $target->sendMessage($sender->getName()." Wants to be allys! please do /accept to accpet thier allyship request!");
                 $task = new accept($this, $target);
 		 	$this->getServer()->getScheduler()->scheduleDelayedTask($task, 600);
+		 	return true;
             }
-            //more todo
+                }else{
+                	$sender->sendMessage("Please do this command in game!");
+                	return false;
+                }
+                }
+ }
+}
+            
         
